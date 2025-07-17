de2687af3e9097b4ee837da5ef3fc5c3ce532435
richard030396 <richardd030396@gmail.com>
2025-07-17 13:39:47 +0530

    First Ever 12 May Batch Code

---
+++ src/main/java/GitHuB/Upload/App.java
@@ -0,0 +1,13 @@
+package GitHuB.Upload;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/com/GitHub/Addition.java
@@ -0,0 +1,15 @@
+package com.GitHub;
+
+public class Addition {
+	
+	public static void main(String args[])
+	{
+		int a=10 ,b=20,c=0;
+		
+		c=a+b;
+		
+		System.out.println("Value of c is " + c);
+		
+	}
+
+}
