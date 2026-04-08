938b03bad588ac45b4f6393a5787ef725d3d0f7a
Richard <richardd030396@gmail.com>
2026-04-08 06:38:32 +0530

    Update Addition.java

--- src/test/java/com/GitHub/Addition.java
+++ src/test/java/com/GitHub/Addition.java
@@ -4,12 +4,11 @@ public class Addition {
 	
 	public static void main(String args[])
 	{
-		int a=10 ,b=20,c=0,s=0;
-		
+		int a=10 ,b=20,c=0;		
 		c=a+b;
-		s=a-b;
+
 		System.out.println("Value of c is " + c);
-		System.out.println("Value of c is "+s);
+
 		
 	}
 
